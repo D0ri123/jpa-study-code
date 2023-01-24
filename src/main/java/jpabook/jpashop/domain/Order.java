390b19442bd127b8ce7d6589733fb5cb3004bd88
Seolhui Lee <94590894+D0ri123@users.noreply.github.com>
2023-01-24 12:17:56 +0900

    feat : 주문 도메인 개발(#2) (#13)
    엔티티에 비즈니스 로직 메소드 생성
    주문 검색 엔티티와 관련 메소드 생성

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -4,11 +4,17 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import static javax.persistence.FetchType.*;
 
 @Entity
 @Table(name = "orders")
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
     @Id @GeneratedValue
@@ -19,6 +25,7 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
+    //cascade 참조는 하나의 엔티티에만 종속되어 있는 경우에 사용하는 것이 좋다.
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
@@ -28,6 +35,7 @@ public class Order {
 
     private LocalDateTime orderDate;
 
+    @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
     //== 연관관계 편의 메서드 ==// -> 양방향일 때 한 쪽에서 편의 메서드를 작성해주는 것이 좋다.
@@ -44,5 +52,51 @@ public class Order {
     public void setDelivery(Delivery delivery){
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    //==생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem...orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        for(OrderItem orderItem : orderItems){
+            order.addOrderItem(orderItem);
+        }
+
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel(){
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem : orderItems){
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice(){
+//        int totalPrice = 0;
+//        for(OrderItem orderItem : orderItems){
+//            totalPrice += orderItem.getTotalPrice();
+//        }
+//        return totalPrice;
+        return orderItems.stream()
+            .mapToInt(OrderItem::getTotalPrice)
+            .sum();
     }
 }
--- src/main/java/jpabook/jpashop/exception/NotEnoughStockException.java
+++ src/main/java/jpabook/jpashop/exception/NotEnoughStockException.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.exception;
 public class NotEnoughStockException extends RuntimeException {
 
     public NotEnoughStockException(){
+        super();
     }
 
     public NotEnoughStockException(String message){
